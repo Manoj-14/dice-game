391ce89add950a64a59f0b08df8f32aa2a1028db
Manoj-14 <manumadhu1425@gmail.com>
2021-03-14 19:55:25 +0530

    added loader

--- diceGame.js
+++ diceGame.js
@@ -1,18 +1,22 @@
+const image1 = document.querySelector('.img-1');
+const image2 = document.querySelector('.img-2');
 const refresh = document.getElementById("refresh");
-refresh.addEventListener("click", play);
+refresh.addEventListener("click", function (e) {
+    image1.setAttribute("src", "loader/loader.gif");
+    image2.setAttribute("src", "loader/loader.gif");
+    setTimeout(play, 750);
+});
 // for player 1
-function play(e){
+function play(e) {
     const randamNum1 = Math.floor(Math.random() * 6) + 1;
     const diceImgName1 = `dice-${randamNum1}.png`;
     const diceImage1 = `dice-imgs/${diceImgName1}`;
-    const image1 = document.querySelector('.img-1');
     image1.setAttribute("src", diceImage1);
 
     // for player 2
     const randamNum2 = Math.floor(Math.random() * 6) + 1;
     const diceImgName2 = `dice-${randamNum2}.png`;
     const diceImage2 = `dice-imgs/${diceImgName2}`;
-    const image2 = document.querySelector('.img-2');
     image2.setAttribute("src", diceImage2);
 
     // choosing winner and loser
